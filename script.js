ed13169422f3aef7c2c9f7a0952211fccbc3f194
Kenneth-joseph <josephochieng812@gmail.com>
2019-11-11 12:38:01 +0300

    added my js value get and text get

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
+var Order=function(size,crust,topping){
+    this.name=name;
+    this.size=size;
+    this.crust=crust;
+    this.topping=topping
+    // this.price=price;
+    
+    Order.prototype.Tprice= function()
+    {
+    return this.size + this.crust + this.topping
+    }
+    
+}
+
+
 $("document").ready(function(){
     $("button.book").click(function(event){
-       var pSize= $("#size option:selected").val();
-       var pName= $("#name option:selected").val();
-       var pCrust = $("#crust option:selected").val();
-       
-       alert(pSize); 
+        event.preventDefault()
+       var pSize=parseInt($("#size option:selected").val()); 
+       var pCrust =parseInt($("#crust option:selected").val()); 
+       var pTopping= parseInt($("#topping option:selected").val());
+
+    let nOrder = new Order(pSize,pCrust,pTopping);
+
+    let price= (nOrder.Tprice());
+    // alert(price);
+
+
+      var Tsize= $("#size option:selected").text();
+      var Tcrust= $("#crust option:selected").text();
+      var Ttopping= $("#topping option:selected").text();
+      var Tname= $("#name option:selected").text();
+
+      
 
     });
+
+
    
 });
 
